0e296ccf59ede7e023689659a1adc9e4065a1074
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-03 09:38:15 -0500

    add user_types schema

--- server/db/migrations/20230303141812_init.js
+++ server/db/migrations/20230303141812_init.js
@@ -2,14 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return Promise.all([
+        knex.schema.createTable('user_types', function (table) {
+            table.increments('id').primary();
+            table.string('name').notNullable();
+        }),
+    ]);
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
