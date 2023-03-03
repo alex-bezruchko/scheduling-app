855d463987dad4c36ae7491f39ef1db0e9402bf2
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-03 18:59:21 -0500

    add correct variables to knexfile

---
+++ server/knexfile.js
@@ -0,0 +1,61 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.APP_DATABASE || 'scheduler',
+      user: process.env.APP_USERNAME || 'abezruchko',
+      password: process.env.APP_PASSWORD || '',
+      port: process.env.POSTGRES_PORT,
+      host: process.env.POSTGRES_HOST
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
