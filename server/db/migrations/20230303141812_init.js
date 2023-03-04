319ca1ea737f3a3652a52a3aec3c68b821bd6325
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-04 14:17:26 -0500

    change password column name

--- server/db/migrations/20230303141812_init.js
+++ server/db/migrations/20230303141812_init.js
@@ -13,6 +13,7 @@ exports.up = function (knex) {
             table.increments('id').primary();
             table.string('username').notNullable();
             table.string('email').notNullable();
+            table.string('password_hash').notNullable();
             table.string('first_name').notNullable();
             table.string('last_name').notNullable();
             table.string('street_address').notNullable();
