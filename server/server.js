4f9cd0cd41d9303a4f96e54ff7b6df72e2816759
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-03 19:04:12 -0500

    update server.js file with cors and basic home route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
+const express = require('express')
+const cors = require('express')
 
-app.listen(5000, () => {
-    console.log('server started at port 5000')
+const port = process.env.POSTGRES_PORT || 3000;
+
+const app = express()
+app.use(express.json({ limit: '5MB', extended: true })); // express.json is replacement for bodyparser , Include extra params {} if needed 
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello from express server')
 })
+
+// Routes goes here
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
