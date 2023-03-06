c0b9443cc350a16f49964821765e003d58f5cc3f
AlexBezruchko <alex.bezruchko@gmail.com>
2023-03-06 09:19:51 -0500

    remove unused mobile navbar menu

--- client/src/components/sections/Navbar.js
+++ client/src/components/sections/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
                             <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Scheduler</Link>
                         </div>
                         <div className="flex items-center">
-                            <div className="hidden md:block">
+                            <div className="md:block">
                                 <div className="relative inline-block text-left">
                                     <button type="button" className="flex text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" id="options-menu" aria-expanded="true" aria-haspopup="true" onClick={toggleDropdown}>
                                         My Account
@@ -86,29 +86,12 @@ const Navbar = () => {
                                 </div>
                             </div>
 
-                            <div className="mr-2 flex md:hidden">
-                                <button onClick={toggleNavbar} type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                    <span className="sr-only">Open main menu</span>
-                                    <svg className={`${isOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                                    </svg>
-                                    <svg className={`${isOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
 
-                {/* <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
-                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Account</a>
-                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Appointment</a>
-
-                    </div>
-                </div> */}
             </nav>
+
         </>
     )
 }
